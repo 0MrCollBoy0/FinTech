425994cb79e03485e889baed9432996c3bab940b
Mustafa Bekirov <mubesad@mail.ru>
2023-12-01 18:42:35 +0300

    Изменил валидации в payment.rb

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,14 @@
 class Payment < ApplicationRecord
   belongs_to :user
   validates :title, presence:true
-  validates :amount, presence:true
+  validates :amount, presence:true, numericality: { greater_than: 0 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validate :receiver
+
+  private
+  def receiver
+    unless User.find_by(email: email)
+      errors.add(:base, 'Пользователь не найден')
+    end
+  end
 end
